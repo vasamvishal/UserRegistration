efb011b331ab99f11f253bafbc4cd059e050e4a6
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-28 15:12:40 +0530

    ADD:Code for email validation

--- userRegistration.js
+++ userRegistration.js
@@ -1,7 +1,7 @@
 var readLine=require('readline-sync');
-let userName=readLine.question("Enter the name");
-let userNameValidator=RegExp('^[A-Z]{1}[a-zA-Z]{2,}');
-let validate=userNameValidator.test(userName);
+let userEmail=readLine.question("Enter the email");
+let emailPattern=RegExp('^[0-9a-zA-Z]+([-,_,+,.]{1}[0-9A-Za-z]+){0,1}@[0-9A-Za-z]+.[A-Za-z]{1,3}(.[a-zA-Z]{1,3}){0,1}$');
+let validate=emailPattern.test(userEmail);
 if(validate==true)
 {
 console.log("T");
