2027b8759d6679b1ba4a05e5d22c8bb24c72f5b1
vaibhavyesalwad <vaibhavyesalwad@gmail.com>
2019-12-28 15:19:57 +0530

    ADD:Code for mobile pattern case

--- userRegistration.js
+++ userRegistration.js
@@ -1,7 +1,9 @@
 var readLine=require('readline-sync');
-let userEmail=readLine.question("Enter the email");
+//let userEmail=readLine.question("Enter the email");
+let userPhoneNumber=readLine.question("Enter the phone number");
 let emailPattern=RegExp('^[0-9a-zA-Z]+([-,_,+,.]{1}[0-9A-Za-z]+){0,1}@[0-9A-Za-z]+.[A-Za-z]{1,3}(.[a-zA-Z]{1,3}){0,1}$');
-let validate=emailPattern.test(userEmail);
+let phonenumber=RegExp('^[6-9]{1}[0-9]{9}$');
+let validate=phonenumber.test(userPhoneNumber);
 if(validate==true)
 {
 console.log("T");
